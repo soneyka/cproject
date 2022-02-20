6e6c1da915ed2c27445441e481bd787c9c052d52
Liana Mk <m.1iana@mail.ru>
2022-02-20 23:22:19 +0300

    Каркас серверной части приложения

---
+++ src/main/java/com/example/cproject/CprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.cproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cproject/CprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
